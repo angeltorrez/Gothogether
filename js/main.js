3fd8f35dc60e11e4f7c316de62dbcd7814fa4965
Angel Torrez <angeltorrez113@gmail.com>
2025-03-08 14:35:31 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Gracias por contactarnos. Nos pondremos en contacto contigo pronto.');
+});
